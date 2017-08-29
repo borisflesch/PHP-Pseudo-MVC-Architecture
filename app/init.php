c0f20556d48306d892001ced76daf788e0b47be8
bernard-ng <31113941+bernard-ng@users.noreply.github.com>
2017-08-30 01:07:42 +0200

    Update init.php

--- app/init.php
+++ app/init.php
@@ -12,7 +12,12 @@ function importClass($class) {
 spl_autoload_register('importClass');
 
 // db connection
-$db = new PDO('mysql:host=host_of_the_db;dbname=name_of_the_db;charset=utf8', 'root', '');
+try {
+    $db = new PDO('mysql:host=host_of_the_db;dbname=name_of_the_db;charset=utf8', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die("Exception:".$e->getMessage()."a la ligne ".$e->getLine." dans ".$e->getFile());
+}
 
 // Initialize ALL manager classes like :
 $articlesManager = new ArticlesManager($db);
